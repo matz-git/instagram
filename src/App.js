76e0aa6a8c9ede6c80262bdecca2195ddfdecf17
matz-git <s.steil2@gmail.com>
2021-06-11 15:33:49 +0200

    Add login - signup page with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 
-function App() {
+const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/sign-up'));
+const NotFound = lazy(() => import('./pages/not-found'));
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Suspense fallback={<p>Loading ... </p>}>
+            <Switch>
+                <Route path={ROUTES.LOGIN} component={Login} />
+                <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                <Route component={NotFound} />
+            </Switch>
+          </Suspense>
+      </Router>
   );
 }
-
-export default App;
