62281d84e640e231c41c12c532f62ab14c1151d7
matz-git <s.steil2@gmail.com>
2021-11-05 17:10:51 +0100

    add is-user-logged-in helper

--- src/pages/login.js
+++ src/pages/login.js
@@ -31,7 +31,7 @@ export default function Login() {
     }, []);
 
     return (
-        <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+        <div className='container flex mx-auto max-w-screen-md items-center h-screen'>st@st.st - ststst
             <div className="flex flex-col w-3/5">
                 <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram App" />
             </div>
